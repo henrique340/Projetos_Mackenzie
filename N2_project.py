4f61cd2d8de1151fcdde843e645632e1697551ec
henrique340 <Henriqueyuji8@gmail.com>
2023-05-08 19:17:47 -0300

    função votar projeto N2

--- N2_project.py
+++ N2_project.py
@@ -1,5 +1,31 @@
-#Funções
+# Listas
+lista_Candidatos_Governador = []
+lista_Partidos_Governador = []
+lista_Cargos_Governador = []
+lista_Voto_Governador = []
 
+lista_Candidatos_Presidente = []
+lista_Partidos_Presidente = []
+lista_Cargos_Presidente = []
+lista_Voto_Presidente = []
+
+lista_Candidatos_Prefeito = []
+lista_Partidos_Prefeito = []
+lista_Cargos_Prefeito = []
+lista_Voto_Prefeito = []
+
+lista_eleitores = []
+
+# Variáveis
+Voto_Branco_Prefeito = 0
+Voto_Branco_Presidente = 0
+Voto_Branco_Governador = 0
+
+Voto_Nulo_Prefeito = 0
+Voto_Nulo_Presidente = 0
+Voto_Nulo_Governador = 0
+
+#Funções
 def Menu():
    print('''1. Cadastrar candidatos
    2. Cadastrar eleitores 
@@ -7,29 +33,34 @@ def Menu():
    4. Apurar resultados
    5. Relatório estatísticas 
    6. Encerrar ''')
-lista_Candidatos_Governador = []
-lista_Candidatos_Presidente = []
-lista_Candidatos_Prefeito = []
-lista_eleitores = []
+
 def Cadastro_Candidatos():
    while True:
-      nome = input('Digite o nome do candidato: ')
-      partido = input('Digite o partido do candidato: ')
+      nome = input('Digite o nome do candidato: ').upper()
       while True:
          cargo = input('Digite o cargo do candidato: ').upper()
-         if cargo == 'GOVERNADOR:':
+         if cargo == 'GOVERNADOR':
+            lista_Partidos_Governador.append(cargo)
             lista_Candidatos_Governador.append(nome)
-            numero = 3 * str(lista_Candidatos_Governador.index(nome))
+            numero = lista_Candidatos_Governador.index(nome)
+            partido = input('Digite o partido do candidato: ')
+            lista_Partidos_Governador.append(partido)
             print(f'O candidato {nome} do numero {numero}, do partido {partido} e do cargo {cargo} foi adicionado com sucesso')
             break
          elif cargo == 'PREFEITO':
+            lista_Cargos_Prefeito.append(cargo)
             lista_Candidatos_Prefeito.append(nome)
-            numero = 2*str(lista_Candidatos_Prefeito.index(nome))
+            numero = lista_Candidatos_Prefeito.index(nome)
+            partido = input('Digite o partido do candidato: ')
+            lista_Partidos_Prefeito.append(partido)
             print(f'O candidato {nome} do numero {numero}, do partido {partido} e do cargo {cargo} foi adicionado com sucesso')
             break
          elif cargo == 'PRESIDENTE':
+            lista_Cargos_Presidente.append(cargo)
             lista_Candidatos_Presidente.append(nome)
-            numero = 1 * str(lista_Candidatos_Presidente.index(nome))
+            numero = lista_Candidatos_Presidente.index(nome)
+            partido = input('Digite o partido do candidato: ')
+            lista_Partidos_Presidente.append(partido)
             print(f'O candidato {nome} do numero {numero}, do partido {partido} e do cargo {cargo} foi adicionado com sucesso')
             break
          else:
@@ -37,9 +68,10 @@ def Cadastro_Candidatos():
       opc = input('Deseja cadastrar outro candidato? [Sim]/[Nao] ').upper()
       if opc == 'NAO':
           break
+
 def Cadastrar_Eleitores():
    while True:
-      nome = input('Digite o nome do eleitor: ')
+      nome = input('Digite o nome do eleitor: ').upper()
       lista_eleitores.append(nome)
       while True:
          cpf = input('Digite o CPF do eleitor: ')
@@ -53,6 +85,63 @@ def Cadastrar_Eleitores():
       if opc == 'NAO':
          break
 
+def Votar():
+     global voto
+     nome = input('Digite o nome do eleitor: ').upper()
+     if nome in lista_eleitores:
+        #prefeito
+        print('-'*45)
+        print('Votação Prefeito')
+        print('-'*45)
+        print('Para votar branco [vote -1]')
+        print('Para votar nulo [vote -2]')
+        voto = int(input('Digite seu voto: '))
+        if voto == '-1':
+           print(f'O eleitor {nome} votou em branco')
+           confirmacao = input('Deseja confirmar o voto: [Sim]/Não] ').upper()
+        elif voto == '-2':
+           print(f'O eleitor {nome} votou nulo')
+           confirmacao = input('Deseja confirmar o voto: [Sim]/[Não] ').upper()
+        else:
+           print(f'O número {voto} é o candidato {lista_Candidatos_Prefeito[voto]} do partido {lista_Partidos_Prefeito[voto]}')
+           confirmacao = input('Deseja confirmar o voto: [Sim]/Não] ').upper()
+
+        #governador
+        print('-' * 45)
+        print('Votação Governador')
+        print('-' * 45)
+        print('Para votar branco [vote -1]')
+        print('Para votar nulo [vote -2]')
+        voto = int(input('Digite seu voto: '))
+            if voto == '-1':
+               print(f'O eleitor {nome} votou em branco')
+               confirmacao = input('Deseja confirmar o voto: [Sim]/Não] ').upper()
+            elif voto == '-2':
+               print(f'O eleitor {nome} votou nulo')
+               confirmacao = input('Deseja confirmar o voto: [Sim]/[Não] ').upper()
+            else:
+               print(f'O número {voto} é o candidato {lista_Candidatos_Governador[voto]} do partido {lista_Partidos_Governador[voto]}')
+               confirmacao = input('Deseja confirmar o voto: [Sim]/Não] ').upper()
+
+        #presidente
+        print('-' * 45)
+        print('Votação Presidente')
+        print('-' * 45)
+        print('Para votar branco [vote -1]')
+        print('Para votar nulo [vote -2]')
+        voto = int(input('Digite seu voto: '))
+        if voto == '-1':
+           print(f'O eleitor {nome} votou em branco')
+           confirmacao = input('Deseja confirmar o voto: [Sim]/Não] ').upper()
+        elif voto == '-2':
+           print(f'O eleitor {nome} votou nulo')
+           confirmacao = input('Deseja confirmar o voto: [Sim]/[Não] ').upper()
+        else:
+           print(f'O número {voto} é o candidato {lista_Candidatos_Presidente[voto]} do partido {lista_Partidos_Presidente[voto]}')
+           confirmacao = input('Deseja confirmar o voto: [Sim]/Não] ').upper()
+     else:
+        print('Digite um nome válido')
+
 
 #sistema
 while True:
@@ -62,6 +151,8 @@ while True:
       Cadastro_Candidatos()
    elif opc == 2:
       Cadastrar_Eleitores()
+   elif opc == 3:
+      Votar()
    elif opc == 6:
       print('Encerrando o programa')
       break
