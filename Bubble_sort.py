7d17e835181ee8d342df6834150d19bc83540421
henrique340 <Henriqueyuji8@gmail.com>
2023-05-11 01:18:01 -0300

    bobble sort melhorado

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,11 +1,11 @@
-def bubbleSort(alist):
-    for num in range(len(alist)-1,0,-1):
+def bubbleSort(list):
+    for num in range(len(list)-1,0,-1):
         for i in range(num):
-            if alist[i]>alist[i+1]:
-                temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] = temp
+            if list[i]>list[i+1]:
+                antigo = list[i]
+                list[i] = list[i+1]
+                list[i+1] = antigo
 
-alist = [54,26,93,17,77,31,44,55,20]
-bubbleSort(alist)
-print(alist)
+list = [54,26,93,17,77,31,44,55,20]
+bubbleSort(list)
+print(list)
