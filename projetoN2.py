34e861200acb0879896832ab1e2bcd836161adbc
henrique340 <Henriqueyuji8@gmail.com>
2023-05-12 23:01:24 -0300

    tentenado arrumar a função votar do projeto N2

--- projetoN2.py
+++ projetoN2.py
@@ -14,70 +14,6 @@ lista_Voto_Prefeito = []
 lista_eleitores = []
 
 
-
-# Funções
-def Menu():
-    print('''1. Cadastrar candidatos
-   2. Cadastrar eleitores 
-   3. Votar
-   4. Apurar resultados
-   5. Relatório estatísticas 
-   6. Encerrar ''')
-
-
-def Cadastro_Candidatos():
-    while True:
-        nome = input('Digite o nome do candidato: ').upper()
-        while True:
-            cargo = input('Digite o cargo do candidato: ').upper()
-            if cargo == 'GOVERNADOR':
-                lista_Candidatos_Governador.append(nome)
-                numero = lista_Candidatos_Governador.index(nome)
-                partido = input('Digite o partido do candidato: ')
-                lista_Partidos_Governador.append(partido)
-                print(
-                    f'O candidato {nome} do numero {numero}, do partido {partido} e do cargo {cargo} foi adicionado com sucesso')
-                break
-            elif cargo == 'PREFEITO':
-                lista_Candidatos_Prefeito.append(nome)
-                numero = lista_Candidatos_Prefeito.index(nome)
-                partido = input('Digite o partido do candidato: ')
-                lista_Partidos_Prefeito.append(partido)
-                print(
-                    f'O candidato {nome} do numero {numero}, do partido {partido} e do cargo {cargo} foi adicionado com sucesso')
-                break
-            elif cargo == 'PRESIDENTE':
-                lista_Candidatos_Presidente.append(nome)
-                numero = lista_Candidatos_Presidente.index(nome)
-                partido = input('Digite o partido do candidato: ')
-                lista_Partidos_Presidente.append(partido)
-                print(
-                    f'O candidato {nome} do numero {numero}, do partido {partido} e do cargo {cargo} foi adicionado com sucesso')
-                break
-            else:
-                print('Insira um cargo válido: ')
-        opc = input('Deseja cadastrar outro candidato? [Sim]/[Nao] ').upper()
-        if opc == 'NAO':
-            break
-
-
-def Cadastrar_Eleitores():
-    while True:
-        nome = input('Digite o nome do eleitor: ').upper()
-        lista_eleitores.append(nome)
-        while True:
-            cpf = input('Digite o CPF do eleitor: ')
-            if len(cpf) == 11:
-                lista_eleitores.append(cpf)
-                print(f'o eleitor {nome} de cpf {cpf} foi adicionado com sucesso')
-                break
-            else:
-                print('Digite um numero de cpf válido')
-        opc = input('Deseja cadastrar outro candidato? [Sim]/[Nao] ').upper()
-        if opc == 'NAO':
-            break
-
-
 lista_Voto_Prefeito.append([0]*len(lista_Candidatos_Prefeito))
 lista_Voto_Governador.append([0]*len(lista_Candidatos_Governador))
 lista_Voto_Presidente.append([0]*len(lista_Candidatos_Presidente))
@@ -181,27 +117,3 @@ def Votar():
             print('Nome inválido, tente novamente')
             continue
 
-
-def Resultado():
-    print('-' * 66)
-    print('|            RANKING DO RESULTADO PARA PRESIDENTE                |')
-    print('-' * 66)
-    print('|   Nome   |   Partido  |  Total de votos  |   % votos válidos   |')
-    print('-' * 66)
-    posicao = max(lista_Candidatos_Presidente).index()
-    print(lista_Candidatos_Presidente[posicao])
-
-
-# sistema
-while True:
-    Menu()
-    opc = int(input('Digite sua escolha: '))
-    if opc == 1:
-        Cadastro_Candidatos()
-    elif opc == 2:
-        Cadastrar_Eleitores()
-    elif opc == 3:
-        Votar()
-    elif opc == 6:
-        print('Encerrando o programa')
-        break
